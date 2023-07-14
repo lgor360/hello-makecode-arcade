e5b9de7acfc861af36744cf1633d1ed1ea5adcd6
lgor360 <93474580+lgor360@users.noreply.github.com>
2023-07-14 22:31:48 +0300

    Added support for 4 players!

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
-mp.onButtonEvent(mp.MultiplayerButton.A, ControllerButtonEvent.Pressed, function (player2) {
-	
-})
-mp.onControllerEvent(ControllerEvent.Connected, function (player2) {
-	
-})
+namespace SpriteKind {
+    export const Ball = SpriteKind.create()
+}
 music.play(music.stringPlayable("C5 B A F G A B C5 ", 268), music.PlaybackMode.LoopingInBackground)
 scene.setBackgroundImage(img`
     . . . . . . . . . . . . . . . . 
@@ -23,24 +20,91 @@ scene.setBackgroundImage(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `)
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Player 1`, SpriteKind.Player))
 mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
-mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two))
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(img`
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
-    8 8 8 8 8 1 8 8 8 1 8 8 8 8 8 . 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
-    8 8 8 1 8 8 8 8 8 8 8 1 8 8 8 . 
-    8 8 8 8 1 8 8 8 8 1 1 8 8 8 8 . 
-    8 8 8 8 8 1 1 1 1 1 8 8 8 8 8 . 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player))
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Player 1`, SpriteKind.Player))
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player))
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Three))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player))
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Four))
+// Responsible for the appearance of other players
+forever(function () {
+    if (mp.isConnected(mp.playerSelector(mp.PlayerNumber.Two))) {
+        mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(img`
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+            8 8 8 8 8 1 8 8 8 1 8 8 8 8 8 . 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+            8 8 8 1 8 8 8 8 8 8 8 1 8 8 8 . 
+            8 8 8 8 1 8 8 8 8 1 1 8 8 8 8 . 
+            8 8 8 8 8 1 1 1 1 1 8 8 8 8 8 . 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Player))
+    }
+    if (mp.isConnected(mp.playerSelector(mp.PlayerNumber.Three))) {
+        mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(assets.image`Player 3`, SpriteKind.Player))
+    }
+    if (mp.isConnected(mp.playerSelector(mp.PlayerNumber.Four))) {
+        mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(assets.image`Player 4`, SpriteKind.Player))
+    }
+})
