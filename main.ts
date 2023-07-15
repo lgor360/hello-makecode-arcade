619c993ed7e9ebc4c380fea35e86ff27eced7601
lgor360 <93474580+lgor360@users.noreply.github.com>
2023-07-15 12:11:13 +0300

    Update! Players now control sprites instead of pictures!

--- main.ts
+++ main.ts
@@ -2,16 +2,23 @@ namespace SpriteKind {
     export const Ball = SpriteKind.create()
 }
 function Players () {
-    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Player 1`, SpriteKind.Player))
-    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
-    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Player 2`, SpriteKind.Player))
-    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two))
-    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(assets.image`Player 3`, SpriteKind.Player))
-    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Three))
-    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(assets.image`Player 4`, SpriteKind.Player))
-    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Four))
+    controller.player1.moveSprite(P1)
+    controller.player2.moveSprite(P2)
+    controller.player3.moveSprite(P3)
+    controller.player4.moveSprite(P4)
 }
+function Sprites () {
+    P1 = sprites.create(assets.image`Player 1`, SpriteKind.Player)
+    P2 = sprites.create(assets.image`Player 2`, SpriteKind.Player)
+    P3 = sprites.create(assets.image`Player 3`, SpriteKind.Player)
+    P4 = sprites.create(assets.image`Player 4`, SpriteKind.Player)
+}
+let P4: Sprite = null
+let P3: Sprite = null
+let P2: Sprite = null
+let P1: Sprite = null
 music.play(music.stringPlayable("C5 A B F D A G B ", 101), music.PlaybackMode.LoopingInBackground)
+Sprites()
 scene.setBackgroundImage(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
