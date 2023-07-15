e1b05dc757ab881c33a06b3a55871095637f3dcd
lgor360 <93474580+lgor360@users.noreply.github.com>
2023-07-15 10:01:32 +0300

    Moved the blocks that add players to a separate function.

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 namespace SpriteKind {
     export const Ball = SpriteKind.create()
 }
+function Players () {
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Player 1`, SpriteKind.Player))
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Player 2`, SpriteKind.Player))
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two))
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(assets.image`Player 3`, SpriteKind.Player))
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Three))
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(assets.image`Player 4`, SpriteKind.Player))
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Four))
+}
 music.play(music.stringPlayable("C5 A B F D A G B ", 101), music.PlaybackMode.LoopingInBackground)
 scene.setBackgroundImage(img`
     . . . . . . . . . . . . . . . . 
@@ -20,11 +30,4 @@ scene.setBackgroundImage(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `)
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Player 1`, SpriteKind.Player))
-mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Player 2`, SpriteKind.Player))
-mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two))
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(assets.image`Player 3`, SpriteKind.Player))
-mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Three))
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(assets.image`Player 4`, SpriteKind.Player))
-mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Four))
+Players()
